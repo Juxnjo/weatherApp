abca3f72ac46b43e36b0af3de99092fcf2045e94
“Juan <ramirezjuanj0117@gmail.com>
2023-09-20 17:20:01 -0500

    variable de entorno API

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { CssBaseline } from "@mui/material";
+import { SnackbarProvider } from "notistack";
+
+ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <SnackbarProvider>
+      <CssBaseline />
+      <App />
+    </SnackbarProvider>
+  </React.StrictMode>
 )
