c9cabd04863bc83a0f0f3fe95157ce78011d8090
“Juan <ramirezjuanj0117@gmail.com>
2023-09-20 18:31:18 -0500

    todo OK, pendiente modularizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,67 @@
+import { LoadingButton } from "@mui/lab";
 import { Box, Container, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+const API_WEATHER = `http://api.weatherapi.com/v1/current.json?key=${ import.meta.env.VITE_API_KEY
+  }&lang=es&q=`;
 
 const App = () =>
 {
 
-  const onSubmit = e => {
+  const [ city, setCity ] = useState( '' )
+  const [ loading, setLoading ] = useState( false )
+  const [ error, setError ] = useState( {
+    error: false,
+    message: ""
+  } )
+
+  const [ weather, setWeather ] = useState( {
+    city: '',
+    country: '',
+    temp: ' ',
+    condition: '',
+    icon: '',
+    conditionText: ''
+  } )
+
+  const onSubmit = async ( e ) =>
+  {
     e.preventDefault();
-    console.log('ok')
+    setLoading( true )
+    setError( {
+      error: false,
+      message: ""
+    } )
+
+    try
+    {
+      if ( !city.trim() ) throw { message: 'El campo ciudad es obligatorio' }
+
+      const res = await fetch( `${ API_WEATHER }${ city }` )
+      const data = await res.json()
+
+      if ( data.error ) throw { message: data.error.message }
+
+      setWeather( {
+        city: data.location.name,
+        country: data.location.country,
+        temp: data.current.temp_c,
+        condition: data.current.condition.code,
+        icon: data.current.condition.icon,
+        conditionText: data.current.condition.text
+      } )
+
+    } catch ( error )
+    {
+      setError( {
+        error: true,
+        message: error.message
+      } )
+    } finally
+    {
+      setLoading( false )
+    }
+
   }
 
   return (
@@ -23,10 +79,10 @@ const App = () =>
 
       </Typography>
       <Box
-        sx={{display: 'grid', gap: 2}}
+        sx={ { display: 'grid', gap: 2 } }
         component='form'
         autoComplete="off"
-        onSubmit={onSubmit}
+        onSubmit={ onSubmit }
       >
         <TextField
           id="city"
@@ -35,9 +91,64 @@ const App = () =>
           size="small"
           required
           fullWidth
+          value={ city }
+          onChange={ ( e ) => setCity( e.target.value ) }
+          error={ error.error }
+          helperText={ error.message }
         />
 
+        <LoadingButton
+          type="submit"
+          variant="contained"
+          loading={ loading }
+          loadingIndicator='Cargando...'
+        >
+          Buscar
+        </LoadingButton>
+
       </Box>
+
+      { weather.city && (
+        <Box
+          sx={ {
+            mt: 2,
+            display: 'grid',
+            gap: 2,
+            textAlign: 'center',
+
+          } }>
+          <Typography variant="h4" component='h2'>
+            { weather.city }, { weather.country }
+          </Typography>
+          <Box
+            component='img'
+            alt={ weather.conditionText }
+            src={ weather.icon }
+            sx={ { margin: '0 auto' } }
+          />
+          <Typography variant="h5" component='h3'>
+            { weather.temp } C
+          </Typography>
+          <Typography variant="h6" component='h4'>
+            { weather.conditionText }
+          </Typography>
+        </Box>
+      ) }
+
+
+
+      <Typography
+        textAlign="center"
+        sx={ { mt: 2, fontSize: "10px" } }
+      >
+        Powered by:{ " " }
+        <a
+          href="https://www.weatherapi.com/"
+          title="Weather API"
+        >
+          WeatherAPI.com
+        </a>
+      </Typography>
     </Container>
   )
 }
